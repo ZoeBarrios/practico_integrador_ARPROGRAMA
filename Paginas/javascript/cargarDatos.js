3962c10ac78e87b462eb3c24e5da85b5825adac2
Zoe <zoebarrios1@outlook.com>
2023-04-03 10:42:01 -0300

    Cambiando nombre a PayFast en navbar, footer y empresa.html

--- Paginas/javascript/cargarDatos.js
+++ Paginas/javascript/cargarDatos.js
@@ -1,10 +1,3 @@
-function cargarDatos(){
-    const nombre=document.getElementById('nombres')
-    const apellido=document.getElementById('apellido')
-    const fechaNact=document.getElementById('fechaNac')
-    const sexo=document.getElementById('sexo')
-    const correo=document.getElementById('correo')
-    const numCel=document.getElementById('numCel')
-
-    console.log(nombre,apellido,fechaNact)
+function cargarDatosTarjeta(){
+    
 }
