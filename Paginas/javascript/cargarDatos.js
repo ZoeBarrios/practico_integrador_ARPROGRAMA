7ea0c4ee438f39197df0ae3a89cd0fbeb07f870b
Zoe <zoebarrios1@outlook.com>
2023-04-03 11:36:44 -0300

    Agregando la funcionalidad de pasar datos del localstorage a la tarjeta

--- Paginas/javascript/cargarDatos.js
+++ Paginas/javascript/cargarDatos.js
@@ -1,3 +1,6 @@
-function cargarDatosTarjeta(){
-    
-}
+const nombre=localStorage.getItem('nombre')
+const apellido=localStorage.getItem('apellido')
+
+const nombreCompleto=document.getElementById('nombreCompleto')
+nombreCompleto.textContent=nombre+" "+apellido
+
