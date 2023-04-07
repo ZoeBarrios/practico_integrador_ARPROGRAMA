94eb99502de9fcc559a26ed9d238521f08fbe028
Zoe <zoebarrios1@outlook.com>
2023-04-07 01:05:40 -0300

    Agregando funcionalidad de descargar a pdf

--- Paginas/javascript/cargarDatos.js
+++ Paginas/javascript/cargarDatos.js
@@ -1,6 +1,20 @@
 const nombre=localStorage.getItem('nombre')
 const apellido=localStorage.getItem('apellido')
 
+
 const nombreCompleto=document.getElementById('nombreCompleto')
 nombreCompleto.textContent=nombre+" "+apellido
 
+
+
+$.ajax({
+    url: 'datos.php',
+    type: 'GET',
+    success: function(response) {
+      console.log(response);
+    },
+    error: function(xhr, status, error) {
+      console.error(error);
+    }
+});
+  
